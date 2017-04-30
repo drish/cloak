d4e8f5c2f2b9b6e602efc9c0202b0ce35e82a52b
drish <d.erich@hotmail.com>
2017-04-30 13:14:14 -0400

    test: add decryption test

---
+++ crypt/decrypt_test.go
@@ -0,0 +1,52 @@
+// Copyright © 2017 carlos derich <carlosderich@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package crypt
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+var passphrase = []byte("wozn")
+
+func TestDecrypt(t *testing.T) {
+
+	file, _ := ioutil.TempFile("", "encrypt-test.txt")
+
+	filename := file.Name()
+	defer os.Remove(filename)
+
+	ioutil.WriteFile(filename, []byte(data), 0644)
+
+	Encrypt(filename, passphrase)
+
+	extension := filepath.Ext(filename)
+	name := filename[0 : len(filename)-len(extension)]
+
+	// decrypt file without extension
+	_, err := Decrypt(name, passphrase)
+	if err != nil {
+		t.Fatalf("Decrypt %s: %v", filename, err)
+	}
+
+	defer os.Remove("out")
+
+	if _, err := os.Stat("out"); os.IsNotExist(err) {
+		t.Fatalf("Decrypt couldnt generate output file")
+	}
+
+}
